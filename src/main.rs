9893997f54625e8e72a0bf65cb2788911a036b63
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2020-07-12 22:50:14 +0200

    Use tokio for some async shenanigans

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use anyhow::{anyhow, Context, Result};
+use futures::future::join_all;
+use image::DynamicImage;
 use image::io::Reader;
 use image::imageops::{resize, FilterType};
 use indicatif::{ProgressBar, ProgressStyle};
 use serde_derive::{Deserialize, Serialize};
 use std::ffi::OsStr;
-use std::fs::{copy, create_dir_all, read_to_string, write};
 use std::path::PathBuf;
 use std::collections::HashSet;
 use structopt::StructOpt;
+use tokio::fs::{copy, create_dir_all, read_to_string, write};
+use tokio::task::spawn_blocking;
 use walkdir::{DirEntry, WalkDir};
 
 static RIGAL_TOML: &str = "rigal.toml";
@@ -48,7 +51,7 @@ struct Conversion {
     to: PathBuf,
 }
 
-fn create_config() -> Result<()> {
+async fn create_config() -> Result<()> {
     let config = Config {
         input: PathBuf::from("input"),
         output: PathBuf::from("_build"),
@@ -59,14 +62,58 @@ fn create_config() -> Result<()> {
         resize: None,
     };
 
-    write(PathBuf::from(RIGAL_TOML), toml::to_string(&config)?)?;
+    write(PathBuf::from(RIGAL_TOML), toml::to_string(&config)?).await?;
     println!("Wrote {}.", RIGAL_TOML);
 
     Ok(())
 }
 
-fn build() -> Result<()> {
-    let config: Config = toml::from_str(&read_to_string(PathBuf::from(RIGAL_TOML))
+fn resize_and_save(image: DynamicImage, width: u32, height: u32, path: PathBuf) -> Result<DynamicImage> {
+    let resized = resize(&image, width, height, FilterType::Lanczos3);
+    resized.save(path)?;
+    Ok(image)
+}
+
+async fn process(entry: Conversion, config: &Config, progress_bar: &ProgressBar) -> Result<()> {
+    progress_bar.set_message(&format!("Processing {:?}", entry.from.path()));
+
+    let mut thumbnail_path = PathBuf::from(&entry.to);
+    thumbnail_path.pop();
+    thumbnail_path.push("thumbnails");
+
+    if !thumbnail_path.exists() {
+        create_dir_all(&thumbnail_path).await?;
+    }
+
+    thumbnail_path.push(entry.to.file_name().unwrap());
+
+    let image = Reader::open(entry.from.path())?.decode()?;
+    let width = config.thumbnail.width;
+    let height = config.thumbnail.height;
+
+    let image = spawn_blocking(move || -> Result<DynamicImage> {
+        resize_and_save(image, width, height, thumbnail_path)
+    }).await??;
+
+    if let Some(resize_config) = &config.resize {
+        let width = resize_config.width;
+        let height  = resize_config.height;
+
+        spawn_blocking(move || -> Result<DynamicImage> {
+            resize_and_save(image, width, height, entry.to)
+        }).await??;
+    }
+    else {
+        copy(entry.from.path(), &entry.to).await?;
+    }
+
+    progress_bar.inc(1);
+
+    Ok(())
+}
+
+async fn build() -> Result<()> {
+    let config: Config = toml::from_str(&read_to_string(PathBuf::from(RIGAL_TOML)).await
         .context("Could not open `rigal.toml'.")?)
         .context("`rigal.toml' format seems broken.")?;
 
@@ -114,46 +161,22 @@ fn build() -> Result<()> {
             "{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {msg}",
     ));
 
-    for entry in entries {
-        progress_bar.set_message(&format!("Processing {:?}", entry.from.path()));
-
-        let image = Reader::open(entry.from.path())?.decode()?;
-        let thumbnail = resize(&image, config.thumbnail.width, config.thumbnail.height, FilterType::Lanczos3);
-
-        let mut thumbnail_path = PathBuf::from(&entry.to);
-        thumbnail_path.pop();
-        thumbnail_path.push("thumbnails");
-
-        if !thumbnail_path.exists() {
-            create_dir_all(&thumbnail_path)?;
-        }
-
-        thumbnail_path.push(entry.to.file_name().unwrap());
-        thumbnail.save(thumbnail_path)?;
-
-        if let Some(resize_config) = &config.resize {
-            let resized = resize(&image, resize_config.width, resize_config.height, FilterType::Lanczos3);
-            resized.save(&entry.to)?;
-        }
-        else {
-            copy(entry.from.path(), &entry.to)?;
-        }
-
-        progress_bar.inc(1);
-    }
+    let futures: Vec<_> = entries.into_iter().map(|e| process(e, &config, &progress_bar)).collect();
+    join_all(futures).await;
 
     Ok(())
 }
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     let commands = Commands::from_args();
 
     match commands {
         Commands::Build => {
-            build()?;
+            build().await?;
         }
         Commands::New => {
-            create_config()?;
+            create_config().await?;
         }
     }
 
